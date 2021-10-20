57715e879ee9ed9524252aa834e3deb5c8dcb6ba
Jakub Barabasz <62941129+JakubBarabasz@users.noreply.github.com>
2021-10-20 18:41:29 +0200

    Movement
    Player movement and physics

---
+++ My project/Assets/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator animator;
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+     
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed, 0);         
+    }
+}
